2d1096efa1c55cf1750c667b9f5f2d583b3b1f6c
javad <javadsaeid8@gmail.com>
2025-09-07 19:27:06 +0330

    add target folder to gitignore for customer module

--- customer/src/main/java/DuplicateResourceException.java
+++
@@ -1,9 +0,0 @@
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String string) {
-        super(string);
-    }
-
-    public DuplicateResourceException(String string, Throwable throwable) {
-        super(string, throwable);
-    }
-}
