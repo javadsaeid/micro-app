1aed02f18376cf1d79d746d0a18ee6df3775b75a
javad <javadsaeid8@gmail.com>
2025-09-11 13:53:38 +0330

    add FraudCheckHistory entity

---
+++ fraud/src/main/java/com/javad/fraud/FraudCheckHistory.java
@@ -0,0 +1,22 @@
+package com.javad.fraud;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(name = "fraud_id_sequence", sequenceName = "fraud_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fraud_id_sequence")
+    private Integer id;
+    private Integer customerId;
+    private Boolean isFraud;
+    private LocalDateTime createdAt;
+}
